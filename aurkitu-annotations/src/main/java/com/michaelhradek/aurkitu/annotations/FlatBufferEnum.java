abb6aa84d67648cdd876d2e88138b155fcedecb2
Michael Hradek <mhradek@gmail.com>
2018-07-10 16:34:54 -0700

    Splitting into two modules - annotations and Maven plugin. This will prevent plugin code from making its way into the target artifact.

---
+++ aurkitu-annotations/src/main/java/com/michaelhradek/aurkitu/annotations/FlatBufferEnum.java
@@ -0,0 +1,30 @@
+package com.michaelhradek.aurkitu.annotations;
+
+import com.michaelhradek.aurkitu.annotations.types.EnumType;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author m.hradek
+ *
+ * A FlatBuffer Enum definition applies to either a enum or a union.
+ *
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FlatBufferEnum {
+  EnumStructureType value() default EnumStructureType.ENUM;
+
+    // Defaulting to the smallest size
+    EnumType enumType() default EnumType.BYTE;
+
+    enum EnumStructureType {
+        ENUM, UNION;
+
+        @Override
+        public String toString() {
+            return this.name().toLowerCase();
+        }
+  }
+}
---
+++ aurkitu-annotations/src/main/java/com/michaelhradek/aurkitu/annotations/types/FieldType.java
@@ -0,0 +1,26 @@
+package com.michaelhradek.aurkitu.annotations.types;
+
+/**
+ * These are the possible field types when defining a TypeDeclaration. FieldType.IDENT means that the field is a class which
+ * ins't one of the other supported field types. IDENT is short for `Identifier`. ARRAY is similar to IDENT however, an array
+ * may also be composed or have a type of an IDENT. For example, String[] or List&lt;String&gt;. MAP is a special type which
+ * allows the use translates a Java Map to a List[GeneratedObject] and back.
+ *
+ * @author m.hradek
+ */
+public enum FieldType {
+    BOOL(boolean.class), BYTE(byte.class), UBYTE(byte.class), SHORT(short.class), USHORT(
+            short.class), INT(int.class), UINT(int.class), FLOAT(float.class), LONG(long.class), ULONG(
+        long.class), DOUBLE(double.class), STRING(String.class), IDENT(null), ARRAY(null), MAP(null);
+
+    public Class<?> targetClass;
+
+    FieldType(Class<?> targetClass) {
+        this.targetClass = targetClass;
+    }
+
+    @Override
+    public String toString() {
+        return this.name().toLowerCase();
+    }
+}
