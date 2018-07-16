6ef7098fc94fd283f1806e5059a2b84b5bfa58bf
Michael Hradek <mhradek@gmail.com>
2018-07-15 21:06:05 -0700

    Removing parent.

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@
 
 read -p  "Enter version number: " input
 
+# sign and bundle annotations
 directory=~/.m2/repository/com/michaelhradek/aurkitu-annotations/"$input"/
 ls -l $directory
 cd $directory
@@ -15,6 +16,7 @@ gpg -ab *"$input".pom
 jar -cvf aurkitu-annotations-"$input"-bundle.jar .
 cp *-bundle.jar $directory/../../
 
+# sign and bundle maven plugin
 directory=~/.m2/repository/com/michaelhradek/aurkitu-maven-plugin/"$input"/
 ls -l $directory
 cd $directory
@@ -26,4 +28,15 @@ gpg -ab *"$input".jar
 gpg -ab *"$input".pom 
 
 jar -cvf aurkitu-maven-plugin-"$input"-bundle.jar .
+cp *-bundle.jar $directory/../../
+
+# sign and bundle parent
+directory=~/.m2/repository/com/michaelhradek/aurkitu-parent/"$input"/
+ls -l $directory
+cd $directory
+rm _remote.repositories
+
+gpg -ab *"$input".pom
+
+jar -cvf aurkitu-parent-"$input"-bundle.jar .
 cp *-bundle.jar $directory/../../
