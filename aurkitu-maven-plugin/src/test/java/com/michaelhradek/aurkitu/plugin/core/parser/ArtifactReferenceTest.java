57955e40e751d3ae47d00e3934a7b5514dbf81cf
Michael Hradek <mhradek@gmail.com>
2019-05-09 11:09:48 -0700

    Removed unused code.

--- aurkitu-maven-plugin/src/test/java/com/michaelhradek/aurkitu/plugin/core/parser/ArtifactReferenceTest.java
+++ aurkitu-maven-plugin/src/test/java/com/michaelhradek/aurkitu/plugin/core/parser/ArtifactReferenceTest.java
@@ -1,28 +1,10 @@
 package com.michaelhradek.aurkitu.plugin.core.parser;
 
 import com.michaelhradek.aurkitu.plugin.core.parsing.ArtifactReference;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class ArtifactReferenceTest {
-
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    /**
-     * @throws java.lang.Exception
-     */
-    @After
-    public void tearDown() throws Exception {
-
-    }
 
     @Test
     public void testArtifactReference() {
