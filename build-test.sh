ba8e8b7d08a3d5457547aad17e6a9dc427e55053
Michael Hradek <mhradek@gmail.com>
2023-04-24 16:16:41 +0200

    Fixed code warning

--- build-test.sh
+++ build-test.sh
@@ -8,7 +8,7 @@ mvn clean install -T 1C
 
 # Kill script if anything goes wrong in the previous step
 if [[ "$?" -ne 0 ]] ; then
-  echo 'Build and/or tests failed'; exit $rc
+  echo 'Build and/or tests failed'; exit "$rc"
 fi
 
 # Run the integration tests
